887855f81d025e9adc25cd7ba5c7daed89479f20
Tony Bowden <tony@tmtm.com>
2021-08-09 12:17:46 +0300

    Skip President and Deputy President
    They're in the cabinet, but not listed on the Ministers page.

--- bin/diff.rb
+++ bin/diff.rb
@@ -3,5 +3,11 @@
 
 require 'every_politician_scraper/comparison'
 
+SKIP = [ # Not listed on Ministers page
+  ['---', 'Matamela Cyril Ramaphosa', 'President of South Africa'],
+  ['---', 'David Dabede Mabuza', 'Deputy President of South Africa']
+].freeze
+
 diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+                                         .reject { |row| SKIP.include? row }
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
