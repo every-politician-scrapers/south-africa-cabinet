7a746d03a1250aa57e80154aedc5acb887e023e7
Tony Bowden <tony@tmtm.com>
2022-04-06 16:41:36 +0300

    less verbose comparisons

--- bin/compare-premiers.sh
+++ bin/compare-premiers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bundle exec ruby bin/scraper/premiers-wikipedia.rb | ifne tee data/premiers-wikipedia.csv
+bundle exec ruby bin/scraper/premiers-wikipedia.rb > data/premiers-wikipedia.csv
 # TODO: bring WD fetch in-place
-bundle exec ruby bin/diff-premiers.rb | ifne tee data/diff-premiers.csv
+bundle exec ruby bin/diff-premiers.rb > data/diff-premiers.csv
